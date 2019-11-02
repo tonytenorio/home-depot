0c8ae7c4ae88d39d65a2f033733548d4282644aa
tony <tonytenorioc@gmail.com>
2019-11-02 11:07:22 -0600

    Slider Site Section completed

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+     /**  First Slider */
+  $(".slider-one")
+  .not(".slick-intialized")
+  .slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    prevArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next"
+  });
+
+});
