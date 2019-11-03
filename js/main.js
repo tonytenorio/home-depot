c26a2c0fe43e9f64bc5e5cf09190e26f804f41a6
tony <tonytenorioc@gmail.com>
2019-11-03 16:39:40 -0600

    Product Slider completed

--- js/main.js
+++ js/main.js
@@ -10,4 +10,15 @@ $(document).ready(function(){
     nextArrow: ".site-slider .slider-btn .next"
   });
 
+  /**  Second  Slider */
+  $(".slider-two")
+    .not(".slick-intialized")
+    .slick({
+      prevArrow: ".site-slider-two .prev",
+      nextArrow: ".site-slider-two .next",
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplaySpeed: 3000,
+    });
+
 });
