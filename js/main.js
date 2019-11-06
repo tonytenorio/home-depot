2e13bdd22556b5b6dcbb233c8daa08c088111167
tony <tonytenorioc@gmail.com>
2019-11-06 10:04:17 -0600

    Feature Section completed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-     /**  First Slider */
-  $(".slider-one")
-  .not(".slick-intialized")
-  .slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    prevArrow: ".site-slider .slider-btn .prev",
-    nextArrow: ".site-slider .slider-btn .next"
-  });
 
+   /**  First Slider */
+   $(".slider-one")
+   .not(".slick-intialized")
+   .slick({
+     autoplay: true,
+     autoplaySpeed: 3000,
+     dots: true,
+     prevArrow: ".site-slider .slider-btn .prev",
+     nextArrow: ".site-slider .slider-btn .next"
+   });
+ 
   /**  Second  Slider */
   $(".slider-two")
     .not(".slick-intialized")
@@ -18,7 +19,19 @@ $(document).ready(function(){
       nextArrow: ".site-slider-two .next",
       slidesToShow: 5,
       slidesToScroll: 1,
-      autoplaySpeed: 3000,
+      autoplaySpeed: 3000
     });
+  
+  /**  Third  Slider */
+  $(".slider-three")
+    .not(".slick-intialized")
+    .slick({
+      prevArrow: ".site-slider-three .prev",
+      nextArrow: ".site-slider-three .next",
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplaySpeed: 3000
+    });
+
 
 });
