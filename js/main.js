d1a05d62a9e4ed005d9b72b23b2aee12c215089a
tony <tonytenorioc@gmail.com>
2019-11-11 09:32:31 -0600

    Project complete all the sections ready

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
-$(document).ready(function(){
-
-   /**  First Slider */
-   $(".slider-one")
-   .not(".slick-intialized")
-   .slick({
-     autoplay: true,
-     autoplaySpeed: 3000,
-     dots: true,
-     prevArrow: ".site-slider .slider-btn .prev",
-     nextArrow: ".site-slider .slider-btn .next"
-   });
- 
+$(document).ready(function() {
+  const breakpoint=[ {
+      breakpoint: 1280, settings: {
+          slidesToShow: 4
+      }
+  }
+  , {
+      breakpoint: 1009, settings: {
+          slidesToShow: 3
+      }
+  }
+  , {
+      breakpoint: 720, settings: {
+          slidesToShow: 2
+      }
+  }
+  , {
+      breakpoint: 460, settings: {
+          slidesToShow: 1
+      }
+  }
+  ];
+  /**  First Slider */
+  $(".slider-one") .not(".slick-intialized") .slick( {
+      autoplay: true, 
+      autoplaySpeed: 3000, 
+      dots: true, 
+      prevArrow: ".site-slider .slider-btn .prev", 
+      nextArrow: ".site-slider .slider-btn .next"
+  }
+  );
   /**  Second  Slider */
   $(".slider-two")
     .not(".slick-intialized")
@@ -19,34 +37,51 @@ $(document).ready(function(){
       nextArrow: ".site-slider-two .next",
       slidesToShow: 5,
       slidesToScroll: 1,
-      autoplaySpeed: 3000
-  });
-  
+      autoplaySpeed: 3000,
+      responsive: breakpoint
+    });
   /**  Third  Slider */
-  $(".slider-three")
-    .not(".slick-intialized")
-    .slick({
+  $(".slider-three") .not(".slick-intialized") .slick( {
       prevArrow: ".site-slider-three .prev",
-      nextArrow: ".site-slider-three .next",
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      autoplaySpeed: 3000,
-      infinite: false,
+      nextArrow: ".site-slider-three .next", 
+      slidesToShow: 4, 
+      slidesToScroll: 1, 
+      autoplaySpeed: 3000, 
       responsive: breakpoint
-  });
-
+  }
+  );
   /**  Four  Slider */
-  $(".slider-four")
-    .not(".slick-intialized")
-    .slick({
-      prevArrow: ".site-slider-four .prev",
-      nextArrow: ".site-slider-four .next",
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      autoplaySpeed: 3000,
-      infinite: false,
+  $(".slider-four") .not(".slick-intialized") .slick( {
+      prevArrow: ".site-slider-four .prev", 
+      nextArrow: ".site-slider-four .next", 
+      slidesToShow: 4, 
+      slidesToScroll: 1, 
+      autoplaySpeed: 3000, 
       responsive: breakpoint
-  });
+  }
+  );
+  /**  Five Slider */
+  $(".slider-five") .not(".slick-intialized") .slick( {
+      prevArrow: ".slider-brand .prev", 
+      nextArrow: ".slider-brand .next", 
+      slidesToShow: 6, 
+      slidesToScroll: 1, 
+      autoplaySpeed: 3000, 
+      infinite: true, 
+      autoplay:true,
+      responsive: breakpoint
+  }
+  );
+  /**  Six Slider */
+  $(".slider-six") .not(".slick-intialized") .slick( {
+      prevArrow: "", 
+      nextArrow: "", 
+      autoplaySpeed: 3000, 
+      infinite: true, 
+      autoplay: true, 
+      dots: true
+  }
+  );
+}
 
-
-});
+);
